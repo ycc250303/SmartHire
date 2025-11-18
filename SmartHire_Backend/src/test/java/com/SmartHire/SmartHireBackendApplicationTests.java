e53f74e6efd7e335a2c76f4a68ecaa1d19cbc0d1
ycc250303 <2351279@tongji.edu.cn>
2025-11-19 00:41:59 +0800

    fix：修复工作流文件

---
+++ SmartHire_Backend/src/test/java/com/SmartHire/SmartHireBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartHire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHireBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
