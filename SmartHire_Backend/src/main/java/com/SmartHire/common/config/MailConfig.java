289e9805f689fbf9cdef09431fb31547e24caea9
ycc250303 <2351279@tongji.edu.cn>
2025-12-03 23:55:27 +0800

    feat：添加邮箱配置类文件

---
+++ SmartHire_Backend/src/main/java/com/SmartHire/common/config/MailConfig.java
@@ -0,0 +1,35 @@
+package com.SmartHire.common.config;
+
+import java.util.Properties;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+    
+    private final MailProperties mailProperties;
+
+    public MailConfig(MailProperties mailProperties) {
+        this.mailProperties = mailProperties;
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setHost(mailProperties.getHost());
+        sender.setPort(mailProperties.getPort());
+        sender.setUsername(mailProperties.getUsername());
+        sender.setPassword(mailProperties.getPassword());
+
+        Properties props = new Properties();
+        if (mailProperties.getProperties() != null) {
+            props.putAll(mailProperties.getProperties());
+        }
+        sender.setJavaMailProperties(props);
+
+        return sender;
+    }
+}
